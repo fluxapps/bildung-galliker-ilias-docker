9e3f177ffeb956831425071d597edf1f1388ae3d
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2023-01-19 08:57:27 +0100

    2023-01-19-1

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build . --target ilias -t bildung-galliker-ilias-docker/ilias:2023-01-12-1
-docker build . --target nginx -t bildung-galliker-ilias-docker/nginx:2023-01-12-1
+docker build . --target ilias -t bildung-galliker-ilias-docker/ilias:2023-01-19-1
+docker build . --target nginx -t bildung-galliker-ilias-docker/nginx:2023-01-19-1
